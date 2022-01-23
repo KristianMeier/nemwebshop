1da9fc7d14693a8628efeee739a5e896eddf8466
Kristian Meier <Kristiankassomeier@gmail.com>
2022-01-23 17:04:59 +0100

    addet icon

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 ?>
 
 <!DOCTYPE html>
@@ -9,6 +10,7 @@
    	<meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="icon" href="swim_icon.png">
     <title>Webshop</title>
 </head>
 
@@ -27,7 +29,6 @@
                         </tr>
                         </thead>
                     <tbody>
-
                         <tr>
                             <td>Swim Pants</td>
                             <td>90</td>
